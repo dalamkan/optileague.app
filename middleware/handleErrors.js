ee65b6a0ccec41d7e943db68226737267181cfe4
Dimitri Alamkan <30351735+dalamkan@users.noreply.github.com>
2020-09-12 12:16:07 +0200

    Added 404 middleware

--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,11 +1,7 @@
 const handleErrors = (err, req, res, next) => {
-  if (err) {
-    console.error(err);
-    res.locals.err = err;
-    res.status(500).render('frError.ejs');
-  } else {
-    res.status(404).render('fr404.ejs');
-  }
+  console.error(err);
+  res.locals.err = err;
+  res.status(500).render('frError.ejs');
 };
 
 module.exports = handleErrors;
