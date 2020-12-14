a5ccfc9ebb3bb3d01d8b841d85c0955699b4b907
Dimitri Alamkan <30351735+dalamkan@users.noreply.github.com>
2020-12-14 11:25:49 +0100

    modified price

--- routes/frAccountRoutes.js
+++ routes/frAccountRoutes.js
@@ -106,7 +106,7 @@ router.post('/create-checkout-session', async (req, res) => {
     mode: "payment",
     client_reference_id: req.userId,
     line_items: [
-      { price: config.STRIPE_PRICE_EUR_24,
+      { price: config.STRIPE_PRICE_EUR_36,
         quantity: 1 }
     ],
   });
